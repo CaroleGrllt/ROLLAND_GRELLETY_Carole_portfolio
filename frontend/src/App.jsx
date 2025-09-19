196b520060d2a74af27c912a63176e6901e8e8d6
Carole ROLLAND GRELLETY <carole.grellety@gmail.com>
2025-09-19 17:05:55 +0200

    Changements dossier 'public'

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Layout from './layouts/Layout.jsx';
 import Home from './pages/Home.jsx';
-import Project from './pages/Project.jsx';
 
 export default function App() {
   const router = createBrowserRouter([
@@ -12,10 +11,6 @@ export default function App() {
 						{
 							index: true,
 							element: < Home />,
-						},
-						{
-							path: '/project',
-							element: < Project />,
 						},
 						{
 							path: '*',
